a9c269ec99cdc1c35d0409d02c9981c618b42865
Supriyo Das <das.supriyo07@gmail.com>
2025-01-21 05:06:33 +0530

    Comparision Query Parameter

--- 05.comparison_query_parameter/controller/app.controller.js
+++ 05.comparison_query_parameter/controller/app.controller.js
@@ -1,5 +1,11 @@
 const { dbModel } = require("../module/db.module");
 
+exports.homeRoute = (req, res) => {
+  res.send(
+    "find data using comparison query parameter in the URL like /product?price=15000"
+  );
+};
+
 exports.sendData = async (req, res) => {
   try {
     const newProduct = new dbModel({
@@ -8,6 +14,17 @@ exports.sendData = async (req, res) => {
       desc: req.body.desc,
     });
     const saveProduct = await newProduct.save();
+    res.status(200).send(saveProduct);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+exports.receiveData = async (req, res) => {
+  try {
+    const price = req.query.price;
+    const allProduct = await dbModel.find({ price: { $gt: price } });
+    res.status(200).send(allProduct);
   } catch (error) {
     res.status(500).send(error.message);
   }
--- 05.comparison_query_parameter/routes/app.route.js
+++ 05.comparison_query_parameter/routes/app.route.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const { sendData } = require("../controller/app.controller");
+const {
+  sendData,
+  receiveData,
+  homeRoute,
+} = require("../controller/app.controller");
 const route = express.Router();
 
-route.get("/", sendData);
+route.get("/", homeRoute);
+route.get("/products", receiveData);
+route.post("/products", sendData);
 
 module.exports = route;
